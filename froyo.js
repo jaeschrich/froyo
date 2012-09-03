ddcc56768c81c18357671b95886161508aa9ae5f
PyScripter255 <pyscripter@hotmail.com>
2012-09-03 16:57:55 -0300

    Update froyo.js

--- froyo.js
+++ froyo.js
@@ -5,24 +5,25 @@ ALL OF THE FOLLOWING IS PUBLIC DOMAIN
 */
 
 var http = require("http"); //Add the http module
-var url = require("url");
+var url = require("url");//Add the url module
 
 function scoop(map, port){ // Deines the main function, scoop.
     if (typeof port === "undefined") { port = 8080; }
     http.createServer(function(req, res) {//creates the server
-    var path = url.parse(req.url).pathname;
-    console.log(req.method+" request recieved for "+path);
-    try {
+    var path = url.parse(req.url).pathname; // Defines the path
+    console.log(req.method+" request recieved for "+path); //Cmd sugar
+    try { //Trys to route the request
         map[path](req, res);
     }
     catch (e){
         try {
-            map['404'](req, res);
+            map['404'](req, res); //Tries to use the user defined 404 function
         }
         catch(e){
-            res.writeHead(200, {"Content-Type": 'text/html'});
+            res.writeHead(200, {"Content-Type": 'text/html'}); //Built-in 404
             res.end("404: Not Found");
         }
+        console.log("404 error for "+path) // console.log the 404 error
     }
     }).listen(port); //Listens at the port specified
     console.log("Server Started at port "+ port);//More cmd sugar text
