04316d46bd55d5cf847c41edc95d8e20b008bd28
PyScripter <jaeschrich@gmail.com>
2012-09-03 15:52:19 -0400

    Complete re-design. Backwards compatable

--- froyo.js
+++ froyo.js
@@ -4,33 +4,28 @@ Froyo.js, a micro-framework for node.js.
 ALL OF THE FOLLOWING IS PUBLIC DOMAIN
 */
 
-var http = require("http") //Add the http module
+var http = require("http"); //Add the http module
+var url = require("url");
 
 function scoop(map, port){ // Deines the main function, scoop.
     if (typeof port === "undefined") { port = 8080; }
-    var a = false //Sets the a switch
-    var keys = Object.keys(map) // Creates a list of keys
     http.createServer(function(req, res) {//creates the server
-        console.log("Request recieved for "+req.url)// Adds helpful coommand-line output
-        var i = 1 //Sets the i switch
-        while(i <= keys.length) { // Starts the while loop for request handling. Iterates through each key in map
-            var k = keys[i - 1] // Sets the k variable, just short for whatever path we are on
-            if (req.url === k){// Awesome part! Checks if path request and the path given match
-                console.log("Routing request for "+req.url)// More useful cmd stuff
-                map[k](req, res);//Calls the request handler, passing request and response
-                a = true// Sets a to true, (flips switch a)
-                break; // Breaks out of the while loop            
-                                }
-                             i++; // increases the i switch if the above if statement evaluates to false               
+    var path = url.parse(req.url).pathname;
+    console.log(req.method+" request recieved for "+path);
+    try {
+        map[path](req, res);
+    }
+    catch (e){
+        try {
+            map['404'](req, res);
         }
-
-            if(a === true) {// This will always answer with a 404, but fails if another response has already been sent
-                res.writeHead(404, {"Content-Type": 'text/html'})
-                res.end("<!doctype html>\n<html>\n<body>\n404: Not Found. Sorry :(\n</body>\n</html>")// Sends a 404 msg
-
-            }
-    }).listen(port) //Listens at the port specified
-    console.log("Server Started at port "+ port)//More cmd sugar text
+        catch(e){
+            res.writeHead(200, {"Content-Type": 'text/html'});
+            res.end("404: Not Found");
+        }
+    }
+    }).listen(port); //Listens at the port specified
+    console.log("Server Started at port "+ port);//More cmd sugar text
 }
 
-exports.scoop = scoop//Exports the function for use
+exports.scoop = scoop;//Exports the function for use
--- index.js
+++ index.js
@@ -6,19 +6,24 @@ ALL OF THE FOLLOWING  IS PUBLIC DOMAIN
 var froyo = require('./froyo')
 
 function main(req, res){ // A requets handler
-    res.writeHead(200, {"Content-Type": "text/html"})
-    res.end("<!doctype html>\n<html>\n<body>\nHello World\n</body>\n</html>")
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end("<!doctype html>\n<html>\n<body>\nHello World\n</body>\n</html>");
 }
 
 function test(req, res){//Another request handler
-    res.writeHead(200, {"Content-Type": "text/html"})
-    res.end("<!doctype html>\n<html>\n<body>\nTesting, Testing\n</body>\n</html>")
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end("<!doctype html>\n<html>\n<body>\nTesting, Testing\n</body>\n</html>");
 }
 
+function four_o_four(req, res){
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end("404");
+}
 
 var map = {//The JSON object that stores the url mapping
     "/": main,
-    '/test': test
+    '/test': test,
+    '404': four_o_four
 }
 
 froyo.scoop(map, 8080)// Starts the server at port 8080
