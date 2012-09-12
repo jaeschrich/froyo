3e6295604fc5d286f4b7e5a6fd1ff5ce7c0e2547
PyScripter <jaeschrich@gmail.com>
2012-09-11 20:51:29 -0400

    First commit

--- froyo.static.js
+++
@@ -1,9 +0,0 @@
-var fs = require("fs");
-
-exports.staticHandler = function(file, encoding){
-    return function(req, res){
-        res.writeHead(200, {"Content-Type": encoding});
-        var fstream = fs.createReadStream(file);
-        fstream.pipe(res);
-    };
-};
