2685cb3a1bd3fd72bb345da60f0101fc39b1816a
PyScripter255 <pyscripter@hotmail.com>
2012-09-21 16:54:36 -0400

    Junk

--- froyo.core.js
+++ froyo.core.js
@@ -1,53 +1,12 @@
-//A nodejs micro framework. It auto-detects if it is a node cluster worker and adjusts for that.
-//
-//##Install
-//
-//###With npm:
-//<code>
-// npm install froyo
-//</code>
-//
-//###Build from source:
-//Needs Jake
-//
-//<code>
-//git clone git://github.com/PyScripter255/frozen-yogurt.git<br>
-//cd frozen-yogurt<br>
-//jake<br>
-//</code>
-//
 
-
-/* 
-Froyo.js, a micro-framework for node.js.
+yo.js, a micro-framework for node.js.
 
 ALL OF THE FOLLOWING IS PUBLIC DOMAIN
 */
 
 var http = require("http");
 var url = require("url");
-var cluster = require("cluster");
-//##froyo.scoop
-//
-//Takes a map variable and a port variable
-//###Map
-//<code>map</code> is an Javascript Object Literal argument passed to froyo.scoop.<br> In it, you set a url (a string) to a request handler (a function that accepts request and response).<br>
-//<code>
-//function main(req, res){<br>
-//res.writeHead(200, {"Content-Type": "text/html"})<br>
-//res.write("Hello World!")<br>
-//res.end()<br>
-//}
-//
-//var map = {<br>
-//"/": main,<br>
-//'/whatever': whateverFunction,<br>
-//'404': four\_o\_four // You can set an optional 404 handler to replace the built-in one<br>
-//}
-//</code>
-//
-//###Port
-//Port is a number that defaults to 8080
+var cluster = require("cluster")
 function scoop(map, port){
     var prefix = "Server: ";
     if (cluster.isWorker){
